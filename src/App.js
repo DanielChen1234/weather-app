edc8187c0d52b8a366a45f219f8d27e235b6e345
DanielChen1234 <dchen8492@yahoo.com>
2018-10-02 12:07:39 -0400

    installed axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React, {Component} from "react"
+import React, {Component} from 'react'
+import axios from 'axios'
+import Title from './components/Title'
+import Form from './components/Form'
+import Weather from './components/Weather'
+
+const API_KEY = 'd61a335d4a31bd2ef2303cb6f9e4a7fa'
 
 export default class App extends Component {
+
+  getWeather = async () => {
+    const api_call = await axios.get(``)
+  }
+
   render(){
     return(
       <div>
-        
+        <Title />
+        <Form />
+        <Weather />
       </div>
     )
   }
--- src/Components/Title.js
+++ src/Components/Title.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 
-export default class Titles extends Component {
+export default class Title extends Component {
     render(){
         return(
             <div>
