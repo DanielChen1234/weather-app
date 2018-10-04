998114843d03706c42ecf241f93063280c1de56f
DanielChen1234 <dchen8492@yahoo.com>
2018-10-04 12:46:40 -0400

    added url axios weather call

--- src/App.js
+++ src/App.js
@@ -8,8 +8,16 @@ const API_KEY = 'd61a335d4a31bd2ef2303cb6f9e4a7fa'
 
 export default class App extends Component {
 
+  constructor(){
+    super();
+    this.state = {
+      city: undefined,
+      country: undefined
+    }
+  }
+
   getWeather = async () => {
-    const api_call = await axios.get(``)
+    const api_call = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${this.state.city},${this.state.country}&appid=${API_KEY}&units=metric`)
   }
 
   render(){
